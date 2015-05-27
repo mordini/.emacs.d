c87601feed98bf3ff1a8dc06a932d03c41e2f455
mordini <fake@notreal.com>
2015-05-26 21:31:11 -0400

    added tmux compatibility

--- init.el
+++ init.el
@@ -158,7 +158,6 @@
 ;;-----------------;;
 ;;-----------------;;
 
-
 (defadvice show-paren-function
     (after show-matching-paren-offscreen activate)
   ;;"If the matching paren is offscreen, show the matching line in the
@@ -173,6 +172,17 @@
     )
   )
 
+(defadvice terminal-init-screen
+    ;; The advice is named `tmux', and is run before `terminal-init-screen' runs.
+    (before tmux activate)
+  ;; Docstring.  This describes the advice and is made available inside emacs;
+  ;; for example when doing C-h f terminal-init-screen RET
+  "Apply xterm keymap, allowing use of keys passed through tmux."
+  ;; This is the elisp code that is run before `terminal-init-screen'.
+  (if (getenv "TMUX")
+      (let ((map (copy-keymap xterm-function-map)))
+	(set-keymap-parent map (keymap-parent input-decode-map))
+	    (set-keymap-parent input-decode-map map))))
 
 ;;-------------;;
 ;;-------------;;
