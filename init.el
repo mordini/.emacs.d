d364098c5590621e59b0b8c46f2b71e468b0543e
mordini <paul@Big-Mac.local>
2015-07-04 15:56:54 -0400

    Revert 40348e0..4830d52
    This rolls back to commit 40348e03c7b2a3bd3d48577d78fee2f19e6e148e.

--- init.el
+++ init.el
@@ -26,7 +26,6 @@
 		     flycheck
 		     ;;GOD MODE TESTING
 		     god-mode
-		     helm
 		     icicles
 		     impatient-mode
                      htmlize
@@ -68,10 +67,10 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("ELPA"  . "http://tromey.com/elpa/")
-                         ("SC" . "http://joseito.republika.pl/sunrise-commander/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+                         ("SC" . "http://joseito.republika.pl/sunrise-commander/")))
 
 ;; fetch the list of packages available
 (unless package-archive-contents
@@ -102,9 +101,51 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-safe-themes
    (quote
-    (flycheck sunrise-x-tree sunrise-x-tabs sunrise-x-modeline sunrise-x-mirror sunrise-x-loop sunrise-x-checkpoints sunrise-x-buttons s multiple-cursors multi-web-mode monokai-theme magit impatient-mode git-rebase-mode git-commit-mode expand-region color-theme auto-indent-mode auto-complete auto-auto-indent))))
+    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" "a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default)))
+ '(fci-rule-color "#49483E")
+ '(highlight-changes-colors ("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   (("#49483E" . 0)
+    ("#67930F" . 20)
+    ("#349B8D" . 30)
+    ("#21889B" . 50)
+    ("#968B26" . 60)
+    ("#A45E0A" . 70)
+    ("#A41F99" . 85)
+    ("#49483E" . 100)))
+ '(magit-diff-use-overlays nil)
+ '(magit-use-overlays nil)
+ '(package-selected-packages (quote (auto-auto-indent)))
+ ;;'(scroll-all-mode t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
